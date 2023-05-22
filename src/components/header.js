f87c3215e2beb3decba1ee7189f917cd8eb2c3a3
SanderDev <sanderpals@live.nl>
2023-05-22 21:21:36 +0200

    Edited header component

--- src/components/header.js
+++ src/components/header.js
@@ -6,24 +6,22 @@ const Header = (props) => {
     let breadcrumbs = (typeof props.breadcrumbs !== 'undefined' ? props.breadcrumbs : null)
     
     return(
-        <>
-            <div className={ styles.header }>
-                <div className="container">
-                    <div className={ styles.text }>
-                        <h1>{ props.title }</h1>
+        <div className={ styles.header }>
+            <div className="container">
+                <div className={ styles.text }>
+                    <h1>{ props.title }</h1>
 
-                        { breadcrumbs &&
-                            <div className={ styles.breadcrumbs }>
-                                <ul>
-                                    <li><a href="https://portfolio.spals.nl/">Home</a></li>
-                                    <li>Velovo</li>
-                                </ul>
-                            </div>
-                        }
-                    </div>
+                    { breadcrumbs &&
+                        <div className={ styles.breadcrumbs }>
+                            <ul>
+                                <li><a href="LINK">Home</a></li>
+                                <li>Portfolio item</li>
+                            </ul>
+                        </div>
+                    }
                 </div>
             </div>
-        </>
+        </div>
     )
 }
   
