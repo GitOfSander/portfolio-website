72148f7061f070dc94d5ab8cf0a66d0552941882
SanderDev <sanderpals@live.nl>
2023-05-29 17:27:57 +0200

    Navbar onscroll different layout added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from '../assets/scss/components/navbar.module.scss';
 
-const Navbar = () => {    
+const Navbar = () => {
+    const [scroll, setScroll] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = (event) => {
+            setScroll(window.pageYOffset > 50)
+        }
+
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', handleScroll);
+        }
+    }, [])
+
     return(
-        <nav className={ styles.navbar + " navbar navbar-expand-lg" }>
+        <nav className={ styles.navbar + " navbar navbar-expand-lg " + (scroll ? 'dark' : '') }>
             <div className="container-fluid">
                 <a className={ styles.logo + " navbar-brand" } href="#">Sander Pals</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
