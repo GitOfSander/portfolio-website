0b4bad762fc92accdf065c2e10e7554edef01996
SanderDev <sanderpals@live.nl>
2023-05-29 17:26:13 +0200

    Project list component updated css and htmll

--- src/components/projects-list.js
+++ src/components/projects-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from '../assets/scss/components/projects-list.module.scss';
 
@@ -29,22 +30,24 @@ const ProjectsList = () => {
     return(
         <>
             {items.map((item, index) => (
-                <>
-                    <div key={ index } className={ styles.item + ' row' }>
-                        <div className='col-md-4'>
-                            <img className={ styles.image } src={ item.imageUrl } />
+                <div key={ index } className={ styles.item }>
+                    <Link to={ styles.url }>
+                        <div className='row'>
+                            <div className='col-lg-4 col-md-6'>
+                                <img className={ styles.image } src={ item.imageUrl } />
+                            </div>
+                            <div className={ styles.info + ' col-lg-5 col-md-6' + (index%2!=0 ? ' order-lg-first offset-lg-3' : '') }>
+                                <h3 className={ styles.title }>{ item.title }</h3>
+                                <p className={ styles.description }>{ item.description }</p>
+                                <span className={ styles.button }>BEKIJK PROJECT</span>
+                            </div>
                         </div>
-                        <div className={ styles.info + ' col-md-5' + (index%2!=0 ? ' order-first offset-md-3' : '') }>
-                            <h3 className={ styles.title }>{ item.title }</h3>
-                            <p className={ styles.description }>{ item.description }</p>
-                            <span className={ styles.button }>Lees meer</span>
-                        </div>
-                    </div>
+                    </Link>
 
                     { amount != (index + 1) &&
                         <hr />
                     }
-                </>
+                </div>
             ))}
         </>
     )
